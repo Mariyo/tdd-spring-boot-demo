83664c1186d992fb31fbc4992cd9aa5c6a0c21b4
Mario Palka <mario.palka@gmail.com>
2025-01-21 11:12:30 +0000

    podpora *.groovy

--- src/main/resources/templates/index.groovy
+++ src/main/resources/templates/index.groovy
@@ -2,7 +2,7 @@ yieldUnescaped '<!DOCTYPE html>'
 html(lang:'en') {
     head {
         meta('http-equiv':'"Content-Type" ' + 'content="text/html; charset=utf-8"')
-        title('User Registration')
+        title('User')
     }
     body {
         label (for:'message', message)
