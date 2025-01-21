c84fc0c463ef0e465fc6a2a8a678e57224641c4f
Mario Palka <mario.palka@gmail.com>
2025-01-21 10:38:47 +0000

    pridane spring-boot-starter-groovy-templates

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ```bash
 mvn spring-boot:run
 ```
+
+or  start the application from the VScode using debugging mode.
--- src/main/java/eu/mariyo/tdd/spring_demo/HelloController.java
+++ src/main/java/eu/mariyo/tdd/spring_demo/HelloController.java
@@ -1,13 +1,15 @@
 package eu.mariyo.tdd.spring_demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/")
-    public String sayHello() {
-        return "Hello, World!";
+    public String sayHello(Model model) {
+        model.addAttribute("message", "Hello, World!");
+        return "index";
     }
 }
